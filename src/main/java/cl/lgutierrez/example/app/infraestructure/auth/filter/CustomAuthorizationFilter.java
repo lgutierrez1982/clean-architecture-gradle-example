3f390a394b5a4c1ad45b151b9a26f7a9b60d0976
Luis Gutierrez <lgutierrez1982@gmail.com>
2022-02-06 19:26:12 -0300

    Implementar ruta login propia en el sistema
    -Remove code for own login

--- src/main/java/cl/lgutierrez/example/app/infraestructure/auth/filter/CustomAuthorizationFilter.java
+++ src/main/java/cl/lgutierrez/example/app/infraestructure/auth/filter/CustomAuthorizationFilter.java
@@ -41,9 +41,9 @@ public class CustomAuthorizationFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-       if(request.getServletPath().equals("/login")){
-           filterChain.doFilter(request, response);
-       } else {
+//       if(request.getServletPath().equals("/login")){//Implementar la ruta /login
+//           filterChain.doFilter(request, response);
+//       } else {
            String authorizationHeader = request.getHeader(AUTHORIZATION);
             if( authorizationHeader != null && authorizationHeader.startsWith(jwtConfig.getTokenPrefix())) {
                try {
@@ -75,5 +75,5 @@ public class CustomAuthorizationFilter extends OncePerRequestFilter {
                filterChain.doFilter(request, response);
            }
        }
-    }
+    //}
 }
